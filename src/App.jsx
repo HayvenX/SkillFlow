a2d2f6b169c8b5a4eb57d1e8f05fbf9837489e65
Daniil Aksonov <aksunov1@gmail.com>
2026-03-19 17:00:04 +0100

    ✨ feat(App): create temp markup to connect with database

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 function App() {
-  return <div className="app"></div>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const requestType = formData.get('request-type');
+    const inputValue = formData.get('request-details');
+  };
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit} action="#">
+        <label>
+          <input type="radio" name="request-type" value="add" />
+          Додати
+        </label>
+
+        <label>
+          <input type="radio" name="request-type" value="get" />
+          Отримати
+        </label>
+
+        <label>
+          <input type="radio" name="request-type" value="delete" />
+          Видалити
+        </label>
+
+        <input type="text" name="request-details" />
+        <button type="submit">Відправити запрос</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
